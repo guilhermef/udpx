110a5fab33830928ef3d464d0944d327b62c11af
Guilherme Souza <guilherme.souza@tfgco.com>
2017-05-22 20:26:32 -0300

    Delivery receipt

--- client.js
+++ client.js
@@ -2,16 +2,28 @@ const udp = require('dgram')
 const uuid = require('uuid/v4')
 
 const client = udp.createSocket('udp4')
+const id = uuid()
+let count = 0
 
-client.on('message', (msg) => {
-  console.log(`Data received from server : ${msg.toString()}`)
+client.on('message', (msg, info) => {
+  const message = msg.toString()
+  console.log(`Data received from server : ${message}`)
+  if (message === 'ping') {
+    console.log(info)
+    client.send(JSON.stringify({ id, action: 'pong' }), info.port, info.address)
+  }
 })
 
 
-client.send(JSON.stringify({ id: uuid() }), 8080, 'localhost', (error) => {
+client.send(JSON.stringify({ id, action: 'entry' }), 8080, 'localhost', (error) => {
   if (error) {
     client.close()
   } else {
     console.log('Data sent !!!')
   }
 })
+
+setInterval(() => {
+  client.send(JSON.stringify({ id, action: 'jump', count }), 8080, 'localhost')
+  count += 1
+}, 300)
--- server.js
+++ server.js
@@ -3,6 +3,23 @@ const udp = require('dgram')
 const server = udp.createSocket('udp4')
 const clients = {}
 
+function checkAlive(clientId) {
+  const ip = clients[clientId].ip
+  const port = clients[clientId].port
+  if (clients[clientId].currentTimeout) {
+    clearTimeout(clients[clientId].currentTimeout)
+  }
+
+  setTimeout(() => {
+    server.send('ping', port, ip)
+  }, 500)
+
+  clients[clientId].currentTimeout = setTimeout(() => {
+    delete clients[clientId]
+    console.log(`${clientId} disconnected`)
+  }, 1000)
+}
+
 server.on('error', (error) => {
   console.log(`Error: ${error}`)
   server.close()
@@ -12,22 +29,37 @@ server.on('message', (msg, info) => {
   console.log(`Data received from client : ${msg.toString()}`)
   console.log('Received %d bytes from %s:%d\n', msg.length, info.address, info.port)
   const pMsg = JSON.parse(msg.toString())
+  if (pMsg.action === 'entry') {
+    clients[pMsg.id] = {
+      ip: info.address,
+      port: info.port,
+      messageCount: 0,
+      seq: Array(32).fill(true),
+    }
+    checkAlive(pMsg.id)
+    return
+  }
 
-  clients[pMsg.id] = { ip: info.address, port: info.port }
+  if (pMsg.action === 'pong') {
+    checkAlive(pMsg.id)
+    return
+  }
+  const client = clients[pMsg.id]
+  client.seq.unshift(true)
+  client.seq.pop()
+  for (let i = client.messageCount + 1; i < pMsg.count; i += 1) {
+    client.seq.unshift(false)
+    client.seq.pop()
+  }
 
-  server.send('ACK', info.port, info.address, () => {
-  })
+  server.send(
+    JSON.stringify({
+      action: 'ack',
+      seq: client.seq,
+    }),
+    client.port,
+    client.ip
+  )
 })
-
-setInterval(() => {
-  Object.keys(clients).forEach((key) => {
-    const client = clients[key]
-    server.send('ping', client.port, client.ip, (error) => {
-      if (error) {
-        console.log('client down!')
-      }
-    })
-  })
-}, 500)
 
 server.bind(8080)
